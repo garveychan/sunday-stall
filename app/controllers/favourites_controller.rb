dc503fb1359aa2a008e8881367fc38508c12f2fa
Garvey <hello@garvey.codes>
2021-05-25 11:27:30 +1000

    Refactor favourite helper methods

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,7 +1,7 @@
 class FavouritesController < ApplicationController
   def index
-    @favourite_stalls = get_favourite_stalls
-    @favourite_products = get_favourite_products
+    @favourite_stalls = get_favourite(:stalls)
+    @favourite_products = get_favourite(:products)
     if (@favourite_stalls + @favourite_products).empty?
       @stalls = Stall.includes([image_attachment: :blob]).all
       render :none
