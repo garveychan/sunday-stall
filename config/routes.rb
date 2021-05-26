e13cd8b1d5f28e93082dc4d7b491a6b12445864d
Garvey <hello@garvey.codes>
2021-05-26 22:53:49 +1000

    Remove commented routes from refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,7 @@ Rails.application.routes.draw do
       delete 'favourite', to: 'favourites#destroy', on: :member
     end
   end
-  
-  # get '/favourites', to: 'favourites#index', as: 'favourites'
-  # post '/stalls/:id/favourite', to: 'favourites#create', as: 'add_favourite_stall'
-  # delete '/stalls/:id/favourite', to: 'favourites#destroy', as: 'remove_favourite_stall'
-  # post '/stalls/:stall_id/products/:id/favourite', to: 'favourites#create', as: 'add_favourite_product'
-  # delete '/stalls/:stall_id/products/:id/favourite', to: 'favourites#destroy', as: 'remove_favourite_product'
-  
+
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
