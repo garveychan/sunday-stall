544b428f34e5aeaffd73411046f5c47b9fdf07c6
Garvey <hello@garvey.codes>
2021-05-27 12:26:25 +1000

    add sendgrid credentials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,2 @@
 class UsersController < ApplicationController
-  after_action :welcome_email, only: %i[create]
-
-  def create
-  end
-  
-  private
-
-  def welcome_email
-    UserMailer.send_welcome_email(self).deliver_later
-  end
 end
